ba2578c0ef21dec98743d91078a2c5eaf5ddc45c
C-TTech <65463598+C-TTech@users.noreply.github.com>
2020-05-16 19:28:21 +0100

    Create stock_app.py

---
+++ stock_app.py
@@ -0,0 +1,32 @@
+# "SQL_Excercise_Stock_Database below is the file that contains the SQL excercise code and the functions for this app
+# Run in terminal/Powershell
+# import SQL_Excercise_Stock_Database
+
+# --------------------------------------------
+
+# Add a Record to the Database
+# SQL_Excercise_Stock_Database.add_one(400, "Proximity Sensor", 7500)
+
+# --------------------------------------------
+
+# Delete a Record (Use id as a string)
+# SQL_Excercise_Stock_Database.delete_one('6')
+
+# --------------------------------------------
+
+# Add Multiple Records to Database
+
+# multiple_records = [
+#    (5948576, 'Cable Gland', 650),
+#    (44066, 'Solid State Relay', 200)
+#    ]
+# SQL_Excercise_Stock_Database.add_many(multiple_records)
+
+# --------------------------------------------
+
+# Lookup with Where
+# SQL_Excercise_Stock_Database.part_lookup('PNP Sensor')
+
+
+# Show all the Records
+# SQL_Excercise_Stock_Database.show_all()
